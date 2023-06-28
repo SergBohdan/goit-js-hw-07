65fb11ba6ac396ea20cf8e413c4a5683384057e7
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-06-29 01:35:09 +0300

    srart

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
 console.log(galleryItems);
+
+
+// function createGalleryItemMarkup({ preview, original, description }) {
+//   return `
+//     <li class="gallery__item">
+//       <a class="gallery__link" href="${original}">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           data-source="${original}"
+//           alt="${description}"
+//         />
+//       </a>
+//     </li>
+//   `;
+// }
+
+// function openModal(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+  
+//   const imageSrc = event.target.dataset.source;
+//   const instance = basicLightbox.create(`<img width="2000" height="900" src="${imageSrc}" alt="" />`);
+//   instance.show();
+// }
+
+// gallery.innerHTML = galleryItems
+//   .map(item => createGalleryItemMarkup(item))
+//   .join('');
+
+// gallery.addEventListener('click', openModal);
+// console.log(basicLightbox);
